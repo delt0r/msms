b9e217a84893db52b0ca1dad6c3d20295a565809
greg ewing aka delt0r <greg.ewing@gmail.com>
2013-05-29 09:34:08 +0200

    missied an export file

---
+++ src/at/mabs/external/RomanStatExport.java
@@ -0,0 +1,193 @@
+package at.mabs.external;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+import at.mabs.abc.SGA;
+import at.mabs.cmdline.CLDescription;
+import at.mabs.cmdline.CLNames;
+import at.mabs.cmdline.CmdLineParser;
+import at.mabs.config.CommandLineMarshal;
+import at.mabs.gui.MSMSPlay;
+import at.mabs.model.SampleConfiguration;
+import at.mabs.segment.FixedBitSet;
+import at.mabs.segment.InfinteMutation;
+import at.mabs.segment.SegmentEventRecoder;
+import at.mabs.stats.StatsCollector;
+import at.mabs.util.Bag;
+import at.mabs.util.random.Random64;
+
+import java.io.*;
+
+import com.esotericsoftware.yamlbeans.YamlWriter;
+
+/**
+ * more generic for general export scripts.
+ * 
+ * @author bob
+ * 
+ */
+public class RomanStatExport {
+	private List<StatsCollector> collectors;
+	private SampleConfiguration sampleConfig;
+	private boolean plainText = false;
+	private boolean randomizeResamples = false;
+	private String inFileName;
+	private String outFileName;
+	private CommandLineMarshal msmsparser;
+
+	@CLNames(names = { "-msms" }, required = true)
+	@CLDescription("The msms comand line for contex. Need the number of demes and sampling stratagy and -s to be correct. Of course this includes the stats collectors used. Dont forget that each indivd is 2 samples")
+	public void msmsCmdLine(String[] msmsArgs) {
+		msmsparser = new CommandLineMarshal();
+		try {
+			CmdLineParser<CommandLineMarshal> marshel = CommandLineMarshal.getCacheParser();// new
+																							// CmdLineParser<CommandLineMarshal>(msmsparser);
+			marshel.processArguments(msmsArgs, msmsparser);
+			sampleConfig = msmsparser.getSampleConfig();
+			// for (StatsCollector stat : collectionStats)
+			// stat.init(sampleConfig); //FIXME
+			List<StatsCollector> defaultCollectors = msmsparser.getStatsCollectors();
+
+			// this.collectionStats.addAll(defaultCollectors);
+
+		} catch (Exception e) {
+			System.err.println("Error Parsing MSMS comand line. Note that all non msms options must come before the -msms switch.");
+			throw new RuntimeException(e);
+		}
+	}
+
+	@CLNames(names = { "-oText" })
+	@CLDescription("Plain output as per msms stats. Usefull for other ABC setups.")
+	public void setPlainText() {
+		this.plainText = true;
+	}
+
+	@CLNames(names = { "-randomize" })
+	@CLDescription("Randomize the resampling. Still continus sets of SNPs")
+	public void setRandomizeResamples() {
+		this.randomizeResamples = true;
+	}
+
+	@CLNames(names = { "-inFile", "-if" })
+	@CLDescription("The input file name. Reads in $FILENAME where each line is 2 samples (hetro/homo), deme locations are defined by order")
+	public void setInFileName(String inFileName) {
+		this.inFileName = inFileName;
+	}
+
+	@CLNames(names = { "-outFile", "-of" })
+	@CLDescription("Output file. - for std output")
+	public void setOutFileName(String outFileName) {
+		this.outFileName = outFileName;
+	}
+
+	public void run() throws IOException {
+		BufferedReader br = null;//
+		if (inFileName.equalsIgnoreCase("-")) {
+			br=new BufferedReader(new InputStreamReader(System.in));
+		} else {
+			br = new BufferedReader(new FileReader(inFileName));
+		}
+		// first col is the sequence id. since each becomes 2....
+		// we remove -1 cols..
+		Bag<FixedBitSet> list = new Bag<FixedBitSet>();
+		
+		String line = br.readLine();
+		//now init things from the first line. ie snp count ...
+		StringTokenizer st=new StringTokenizer(line);
+		int snpCount=st.countTokens()-1;
+		int sampleCount=sampleConfig.getMaxSamples();//note should be 2x the number of rows.
+		
+		for(int i=0;i<snpCount;i++){
+			list.add(new FixedBitSet(sampleCount));
+		}
+		int leaf=0;
+		while (line != null) {
+			st=new StringTokenizer(line);
+			st.nextToken();//index... we don't care.
+			int snp=0;
+			while(st.hasMoreTokens()){
+				String token=st.nextToken();
+				FixedBitSet fbs=list.get(snp);
+				//System.out.println("I:"+i+"\t"+st.countTokens()+"\t"+sampleCount+"\t"+token);
+				if(fbs!=null){
+					switch(Integer.parseInt(token)){
+					case -1:
+						list.set(snp, null);//drop.
+						break;
+					case 1:
+						fbs.set(leaf*2);
+						break;
+					case 2:
+						fbs.set(leaf*2);
+						fbs.set(leaf*2+1);
+						break;
+					}
+				}
+				snp++;
+			}
+			
+			leaf++;
+			line = br.readLine();
+		}
+		List<InfinteMutation> muts=new ArrayList<InfinteMutation>();
+		for(FixedBitSet fbs:list){
+			if(fbs!=null)
+				muts.add(new InfinteMutation(muts.size(),fbs ));
+		}
+		
+		//so now we work with the output. 
+		int perLocusSnps=(int)msmsparser.getSegSiteCount();
+		int locusCount=msmsparser.getRepeats();
+		Random rand = new Random64();
+		for(int i=0;i<locusCount;i++){
+			int start = i*perLocusSnps;
+			if(randomizeResamples)
+				start=rand.nextInt(muts.size()-perLocusSnps);
+			Bag<InfinteMutation> sublist =new Bag(muts.subList(start, start+perLocusSnps));
+			SegmentEventRecoder ser = new SegmentEventRecoder(sublist, msmsparser.getFoldMutations(), msmsparser.getFoldMutations());
+			for (StatsCollector sc : collectors) {
+				sc.collectStats(ser);
+			}
+		}
+		System.out.println("Stats Output");
+		for(StatsCollector sc:collectors){
+			double[] r=sc.summaryStats();
+			for(double d:r){
+				System.out.print(d+"\t");
+			}
+		}
+		System.out.println();
+		
+	}
+	
+	private static void saveStats(Writer writer, List<StatsCollector> stats) throws IOException {
+		YamlWriter yamlWriter = new YamlWriter(writer);
+		yamlWriter.getConfig().setPrivateFields(true);
+		yamlWriter.getConfig().setPrivateConstructors(true);
+		yamlWriter.write(stats);
+		yamlWriter.close();
+	}
+
+	public static void main(String[] args) {
+		RomanStatExport rse=new RomanStatExport();
+		CmdLineParser<RomanStatExport> parser = null;
+		try {
+			parser = new CmdLineParser<RomanStatExport>(RomanStatExport.class);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			return;
+		}
+
+		try {
+			parser.processArguments(args, rse);
+			rse.run();
+		} catch (Exception e) {
+			System.err.println(parser.longUsage());
+			e.printStackTrace();
+
+		}
+	}
+}
